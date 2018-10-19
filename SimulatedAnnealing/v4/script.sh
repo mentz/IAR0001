7998a8187c2353c5e3fdf19ded560e0e8e3e8335
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-10-18 21:48:20 -0300

    Esqueleto do script

--- SimulatedAnnealing/v4/script.sh
+++ SimulatedAnnealing/v4/script.sh
@@ -12,4 +12,9 @@ do
 			./sa Entradas/uf${entr}-01.cnf $cs $t0 $t1 Saidas/sa_${entr}_${cs}_${t0}_${t1}_${i}.dat
 			./rs Entradas/uf${entr}-01.cnf Saidas/rs_${entr}_${i}.dat
 		done
+		python3 py/media.py Saidas/sa_${entr}_${cs}_${t0}_${t1}_ .dat Saidas/sa_${entr}_${cs}_${t0}_${t1}_media.dat
+		python3 py/plot.py Saidas/sa_${entr}_${cs}_${t0}_${t1}_media.dat Graficos/sa_${entr}_${cs}_${t0}_${t1}_media.png
+		python3 py/media.py Saidas/rs_${entr}_ .dat Saidas/rs_${entr}_media.dat
+		python3 py/plot.py Saidas/rs_${entr}_media.dat Graficos/rs_${entr}_media.png
+	done
 done
